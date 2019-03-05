e66db19fa7531eb676c282c69f2b44977c57980d
Микола Мантицький <slwpke447@gmail.com>
2019-03-05 14:21:06 +0200

    create

---
+++ WebAPI/CodeFirst/DateTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+  public  class DateTable
+    {
+        public int Id { get; set; }
+        public DateTime dateTime1  { get; set; }
+        public DateTime dataTime2 { get; set; }
+    }
+}
